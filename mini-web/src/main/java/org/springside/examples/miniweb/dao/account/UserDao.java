a2f61141e544a8c910893c39f36e5882a2112213
wendypan <panzhiwei@qq.com>
2012-07-04 01:09:46 +0800

    i

--- mini-web/src/main/java/org/springside/examples/miniweb/dao/account/UserDao.java
+++ mini-web/src/main/java/org/springside/examples/miniweb/dao/account/UserDao.java
@@ -1,6 +1,8 @@
 package org.springside.examples.miniweb.dao.account;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springside.examples.miniweb.entity.account.User;
 
 /**
@@ -8,7 +10,7 @@ import org.springside.examples.miniweb.entity.account.User;
  * 
  * @author calvin
  */
-public interface UserDao extends PagingAndSortingRepository<User, String> {
+public interface UserDao extends JpaRepository<User, String>,JpaSpecificationExecutor<User>,QueryDslPredicateExecutor<User> {
 
 	User findByLoginName(String loginName);
 }
